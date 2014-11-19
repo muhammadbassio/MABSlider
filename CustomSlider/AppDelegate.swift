030256b3a3b39f021135200ed3220513d1c84ab3
Muhammad Bassio <muhammadbassio@Muhammads-MacBook-Pro.local>
2014-11-19 21:36:11 +0200

    Initial Commit

---
+++ CustomSlider/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CustomSlider
+//
+//  Created by Muhammad Bassio on 11/19/14.
+//  Copyright (c) 2014 Muhammad Bassio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
